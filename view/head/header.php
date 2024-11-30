d1bf08146f5cc92a4bb28044de6451bc7c819ced
Kattyaqz <kattya-estrella@live.com>
2024-11-30 01:41:35 -0300

    Tarea 5 puntos

--- view/head/header.php
+++ view/head/header.php
@@ -12,35 +12,38 @@
 
 <nav class="navbar navbar-expand-lg bg-body-tertiary">
   <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
+    <a class="navbar-brand" href="#">Moonlab</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNavDropdown">
       <ul class="navbar-nav">
         <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Home</a>
+          <a class="nav-link active" aria-current="page" href="#">IA</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
+          <a class="nav-link" href="#">RV</a>
         </li>
         <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
+          <a class="nav-link" href="#">Programas</a>
         </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown link
+            Otros
           </a>
           <ul class="dropdown-menu">
             <li><a class="dropdown-item" href="#">Productos</a></li>
             <li><a class="dropdown-item" href="#">Contactos</a></li>
-            <li><a class="dropdown-item" href="#">Otros...</a></li>
+            
           </ul>
         </li>        
       </ul>
-      <form class="d-flex ms-auto" role="search">
-            <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-            <button class="btn btn-outline-success" type="submit">Search</button>
+      <form class="d-flex ms-auto" role="Usuario">
+            <input class="form-control me-2" type="Usuario" placeholder="Usuario" aria-label="Usuario">
+            <input class="form-control me-2" type="Password" placeholder="Password" aria-label="Password">
+            <button class="btn btn-outline-success" type="submit">Ingresar</button>
+            <button class="btn btn-outline-success" type="submit">Registrarse</button>
+
       </form>
     </div>
   </div>
